6fd1315b67b23c1237d1910f8d8befd238805379
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-11 23:08:47 -0300

    Modificacion estilos Appbar

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -21,6 +21,9 @@ class AppTheme {
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: colorList[selectedColor]
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: true
+    )
   );
 }
