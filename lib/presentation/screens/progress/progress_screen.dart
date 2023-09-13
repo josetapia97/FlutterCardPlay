921ff81f07e5f74183812f11313d07a1e53edea7
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-13 15:37:21 -0300

    Progress Indicators controlados

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -11,7 +11,6 @@ class ProgressScren extends StatelessWidget {
         title: const Text('Progres Incidcators'),
       ),
       body: const _ProgressView(),
-      
     );
   }
 }
@@ -29,9 +28,9 @@ class _ProgressView extends StatelessWidget {
           SizedBox(height: 10),
           CircularProgressIndicator(strokeWidth: 2),
           SizedBox(height: 20),
-          Text('Indicador de progreso circular controlado'),
+          Text('Indicador de progreso circular y lineal controlado'),
           _ControledProgressIndicator()
-          ],
+        ],
       ),
     );
   }
@@ -42,6 +41,31 @@ class _ControledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return (value * 2) / 100;
+        }).takeWhile((value) => value < 100),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(
+                  value: progressValue,
+                  strokeWidth: 2,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                //linearprogressindicator debe ir dentro de expanded, ya que como esta iniciado dentro de un row,
+                //el row puede crecer a los lados, mientras que el expanded pone un limite
+                Expanded(child: LinearProgressIndicator(value: progressValue))
+              ],
+            ),
+          );
+        });
   }
 }
