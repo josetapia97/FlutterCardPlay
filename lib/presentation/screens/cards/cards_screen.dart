c4215c711d97033495fcf3f3f630427445b71766
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-12 21:06:20 -0300

    Card Type 4: Imagenes

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -36,7 +36,12 @@ class _CardsView extends StatelessWidget {
             (card) => _CardType1(label: card['label'], elevation: card['elevation']),),
             ...cards.map(
             (card) => _CardType2(label: card['label'], elevation: card['elevation']),),
+            ...cards.map(
+            (card) => _CardType3(label: card['label'], elevation: card['elevation']),),
+            ...cards.map(
+            (card) => _CardType4(label: card['label'], elevation: card['elevation']),),
             
+            const SizedBox(height: 50)
             ],
           
       ),
@@ -78,6 +83,7 @@ class _CardType1 extends StatelessWidget {
 
 
 class _CardType2 extends StatelessWidget {
+
   final String label;
   final double elevation;
 
@@ -85,15 +91,57 @@ class _CardType2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Obtener la paleta de colores del tema actual.
     final colors = Theme.of(context).colorScheme;
 
     return Card(
+      // Personalizar la forma de la tarjeta con bordes redondeados y un borde exterior.
       shape: RoundedRectangleBorder(
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        borderRadius: const BorderRadius.all(Radius.circular(12)), // Bordes redondeados.
         side: BorderSide(
-          color: colors.outline
-        )
+          color: colors.outline, // Color del borde exterior.
+        ),
       ),
+      elevation: elevation, // Establecer la elevación de la tarjeta (sombra).
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10), // Añadir relleno al contenido de la tarjeta.
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined), // Icono de "más opciones".
+                onPressed: () {
+                  // Lógica que se ejecuta al hacer clic en el icono.
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'), // Texto con la etiqueta y "outline".
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class _CardType3 extends StatelessWidget {
+  
+  final String label;
+  final double elevation;
+
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      color: colors.surfaceVariant,
       elevation: elevation,
       child: Padding(
         padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
@@ -107,10 +155,50 @@ class _CardType2 extends StatelessWidget {
                 ),
             Align(
               alignment: Alignment.bottomLeft,
-              child: Text('$label - outline'),
+              child: Text('$label - filled'),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20)),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined), onPressed: (){}
+                ),
+            ),
+              
+          )
+        ],
       ),
     );
   }
